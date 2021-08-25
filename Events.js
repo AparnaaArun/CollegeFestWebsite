402cd64170340c83772d3cc403858e95ce338496
AparnaaArun <85065074+AparnaaArun@users.noreply.github.com>
2021-08-25 23:28:27 +0530

    Add files via upload

---
+++ Events.js
@@ -0,0 +1,70 @@
+function ondance() { //to show the overlay as a block
+    document.getElementById("overlaydance").style.display = "block";
+  }
+  
+  function offdance() { //to hide the overlay
+    document.getElementById("overlaydance").style.display = "none";
+  }
+
+  function onlit() { //different functions to avoid overlap of displayed text
+    document.getElementById("overlaylit").style.display = "block";
+  }
+  
+  function offlit() { //different functions to avoid overlap of displayed text
+    document.getElementById("overlaylit").style.display = "none";
+  }
+
+  function onact() {
+    document.getElementById("overlayact").style.display = "block";
+  }
+  
+  function offact() {
+    document.getElementById("overlayact").style.display = "none";
+  }
+
+  function ongame() {
+    document.getElementById("overlaygame").style.display = "block";
+  }
+  
+  function offgame() {
+    document.getElementById("overlaygame").style.display = "none";
+  }
+
+  function onphoto() {
+    document.getElementById("overlayphoto").style.display = "block";
+  }
+  
+  function offphoto() {
+    document.getElementById("overlayphoto").style.display = "none";
+  }
+
+  function onwin() {
+    document.getElementById("overlaywin").style.display = "block";
+  }
+  
+  function offwin() {
+    document.getElementById("overlaywin").style.display = "none";
+  }
+
+  function registrationpage(){ //to open the linked page in a new window
+    window.open("registration.html");
+  }
+
+  function openNav() { //to open overlay from left to right with animation
+    document.getElementById("myNav").style.width = "100%";
+  }  
+
+  function closeNav() { //to close overlay from left to right with animation
+    document.getElementById("myNav").style.width = "0%";
+  }
+
+
+
+
+                  
+
+              
+
+                  
+
+               
